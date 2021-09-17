b0022fec47f1e9424d8d065be976155b42c1455d
Francisco Navarrete III <72310634+navarreteiiifrancisco@users.noreply.github.com>
2021-09-17 14:26:30 -0500

    Update py_calculator.py

--- py_calculator.py
+++ py_calculator.py
@@ -8,8 +8,9 @@ base.title("Simple Calculator")
 e = Entry(base, width= 35, borderwidth= 5)
 e.grid(row= 0, column= 0, columnspan= 3, padx= 10, pady= 10)
 
+# Creating all the functions to perform calculations and task.
+
 def button_click(number):
-    
     current =  e.get()
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
@@ -66,7 +67,7 @@ def button_equal():
         e.insert(0, f_num / int(second_number))
 
 
-# Defining Buttons
+# Defining buttons 0-7.
 button_1 = Button(base, text= "1", padx= 40, pady= 20, command= lambda: button_click(1))
 button_2 = Button(base, text= "2", padx= 40, pady= 20, command= lambda: button_click(2))
 button_3 = Button(base, text= "3", padx= 40, pady= 20, command= lambda: button_click(3))
@@ -78,44 +79,45 @@ button_8 = Button(base, text= "8", padx= 40, pady= 20, command= lambda: button_c
 button_9 = Button(base, text= "9", padx= 40, pady= 20, command= lambda: button_click(9))
 button_0 = Button(base, text= "0", padx= 40, pady= 20, command= lambda: button_click(0))
 
+# Defining buttons +, -, *, and /.
 button_add = Button(base, text= "+", padx= 39, pady= 20, command= button_add)
 button_subtract = Button(base, text= "-", padx= 41, pady= 20, command= button_subtract) 
 button_multiply = Button(base, text= "*", padx= 41, pady= 20, command= button_multiply)
 button_divide = Button(base, text= "/", padx= 39, pady= 20, command= button_divide)
 
+# Defining buttons "=" and "Clear".
 button_equal = Button(base, text= "=", padx= 91, pady= 20, command= button_equal)
 button_clear = Button(base, text= "Clear", padx= 79, pady= 20, command= button_clear)
 
-# Displays buttons on the screen
+# Button locations on the grid.
 
-
-# Buttons on row 1
+# Buttons on row 1.
 button_7.grid(row= 1, column= 0)
 button_8.grid(row= 1, column= 1)
 button_9.grid(row= 1, column= 2)
 
-# Buttons on row 2
+# Buttons on row 2.
 button_4.grid(row= 2, column= 0)
 button_5.grid(row= 2, column= 1)
 button_6.grid(row= 2, column= 2)
 
-# Buttons on row 3
+# Buttons on row 3.
 button_1.grid(row= 3, column= 0)
 button_2.grid(row= 3, column= 1)
 button_3.grid(row= 3, column= 2)
 
-# Buttons on row 4
+# Buttons on row 4.
 button_0.grid(row= 4, column= 0)
 button_clear.grid(row= 4, column= 1, columnspan= 2)
 
-# Buttons on row 5
+# Buttons on row 5.
 button_add.grid(row= 5, column= 0)
 button_equal.grid(row= 5, column= 1, rowspan= 1,columnspan= 2)
 
-# Buttons on row 6
+# Buttons on row 6.
 button_subtract.grid(row= 6,column= 0)
 button_divide.grid(row= 6, column= 1)
 button_multiply.grid(row= 6, column= 2)
 
-
+# runs and displays the entire Tkinter widget. 
 base.mainloop()
